29cf27667b133185773e78b81f7ddde3b5689386
simran2020 <62429410+simran2020@users.noreply.github.com>
2020-09-30 11:56:31 -0700

    Update factorial.py

--- array/factorial.py
+++ array/factorial.py
@@ -6,11 +6,12 @@ Problem Constraints
 
 '''
 
-def factorial(A: int) -> int:
-        if A <= 1:
-            return 1
-
-        return (A * factorial(A-1))
+def factorial(n,memorize = {1:0 , 2:1}):
+        if n in memorize:
+                return memorize[n]
+        else:
+                memorize[n] = facrorial(n-1,memorize)+ factorial(n-2,memorize)
+                return memorize[n]
 
 if __name__ == "__main__":
     A = 3
